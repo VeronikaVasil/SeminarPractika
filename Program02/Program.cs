dbc7d5f3318cf005096a7fe68fd8c30860e5d52f
Veronika <valeria14081981@yandex.com>
2022-08-07 00:01:41 +0300

    Максимальное из трёх целых чисел

---
+++ Program02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Ввести три числа и выдать максимальное из этих чисел
+
+int a, b, c;
+
+Console.WriteLine("Введите три числа: ");
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+ 
+   if (b < a && a > c)
+
+   {
+
+       Console.WriteLine(a);
+    
+     }
+
+     else if (a < b && b > c)
+     {
+
+       Console.WriteLine(b);
+    
+     }
+
+     else if (a < c && c > b)
+     {
+
+     Console.WriteLine(c);
+    
+     }
+      
+     Console.ReadLine();
+
