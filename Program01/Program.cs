c82fe6d7b2cc3655d2beeff63fcee6f4d391571c
Veronika <valeria14081981@yandex.com>
2022-08-06 23:24:56 +0300

    Найти большее из двух чисел

---
+++ Program01/Program.cs
@@ -0,0 +1,27 @@
+﻿//Найти большее из двух чисел
+
+int a, b;
+ 
+Console.WriteLine("Введите два числа : ");
+
+a = Convert.ToInt32(Console.ReadLine());
+ 
+b = Convert.ToInt32(Console.ReadLine());
+ 
+    if (a > b)
+        
+        {
+ 
+            Console.WriteLine("{0} большее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} большее число ", b);
+ 
+        }
+ 
+        Console.ReadLine();
