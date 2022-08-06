42571b1ed903655054be7a3cadaf8073a43fca53
Veronika <valeria14081981@yandex.com>
2022-08-07 02:09:23 +0300

    Все четные числа от 1 до N

---
+++ Program04/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ввести число N, а на выводе показать все четные числа от 1 до N
+
+Console.WriteLine("Введите число N: ");
+
+int numberN = Convert.ToInt32(Console.ReadLine());
+    
+    for (int i = 1;  i <= numberN; i++)
+
+    {
+        if (i % 2 == 0) 
+
+        {
+            Console.Write(i + " ");
+        }
+
+    }
+
+    Console.ReadLine();
