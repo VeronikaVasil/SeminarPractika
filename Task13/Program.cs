b8485af84091d0b8205276edbe021c88b4665066
Veronika <valeria14081981@yandex.com>
2022-08-09 01:17:49 +0300

    3 digit of number

---
+++ Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Вывести третью цифру заданного числа или сообщить, что третьей цифры нет.
+
+int a;
+
+Console.Write("Введите число:" );
+
+
+a = Convert.ToInt32(Console.ReadLine());
+
+
+    if (a < 100)
+
+        {
+            Console.Write("Третьей цифры нет");
+        }
+
+     else if (a > 100)
+         
+        {
+            string str = a.ToString();
+            Console.Write("{0} третья цифра", (str[2]));
+        }
+        
+
