f43c1dfe7e7a5ff64ff4e1ff541d179c07abb698
Veronika <valeria14081981@yandex.com>
2022-08-10 18:30:02 +0300

    Добавление digit

---
+++ Task13_02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вывести третью цифру заданного числа или сообщить, что третьей цифры нет.
+
+Console.Write("Введите число:" );
+
+ int digitnumber = Convert.ToInt32(Console.ReadLine());
+
+    if (digitnumber < 100)
+        {
+            Console.Write("Третьей цифры нет");
+        }
+     else if (digitnumber > 100)
+        {
+            string stringnumber = Convert.ToString(digitnumber);
+            Console.Write("{0} третья цифра", (stringnumber[2]));
+        }
+
+Console.ReadLine();
+
+
