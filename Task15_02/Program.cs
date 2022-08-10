67a1a585b4b496e144dbd52e28712c8b1e770d80
Veronika <valeria14081981@yandex.com>
2022-08-10 18:01:51 +0300

    Другой вариант решения с добавлением переменной

---
+++ Task15_02/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число, обозначающее день недели: " );
+
+int weekday = Convert.ToInt32(Console.ReadLine()); 
+
+
+    if (weekday == 6 || weekday == 7)
+
+    {
+        Console.Write("Этот день является выходным"); 
+    }
+    
+    else if ( weekday < 6)
+
+    {
+        Console.Write("Это рабочий день недели");
+    }    
+
+Console.ReadLine();
+
+
