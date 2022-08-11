d6cc9587dbecff29bbcc87ac47a8366eda4d4272
Veronika <valeria14081981@yandex.com>
2022-08-12 02:39:27 +0300

    Таблица кубов чисел от 1 до N

---
+++ Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите целое положительное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+     while (count <= N)
+    {
+       Console.Write($"{count * count * count} ");
+       count ++;
+    }
