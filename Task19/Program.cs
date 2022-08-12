67463dbfb2b81977154e98d29da49bfbd1f085b1
Veronika <valeria14081981@yandex.com>
2022-08-12 11:38:03 +0300

    Является ли пятизначное число палиндромом

---
+++ Task19/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+
+string DigitNumber = Console.ReadLine();
+
+
+if (DigitNumber[0] == DigitNumber[4] || DigitNumber[1] == DigitNumber[3])
+  
+  {
+    Console.WriteLine($"Ваше число: {DigitNumber} - палиндром.");
+  }
+
+  else Console.WriteLine($"Ваше число: {DigitNumber} - не является палиндромом.");
