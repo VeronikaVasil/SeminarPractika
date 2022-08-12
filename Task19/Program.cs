774c532b837e2572066532758582bff7d188ac80
Veronika <valeria14081981@yandex.com>
2022-08-12 14:30:09 +0300

    Исправила союз ||на союз  &&

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,7 +4,7 @@ Console.Write("Введите пятизначное число: ");
 
 string DigitNumber = Console.ReadLine();
 
-if (DigitNumber[0] == DigitNumber[4] || DigitNumber[1] == DigitNumber[3])
+if (DigitNumber[0] == DigitNumber[4] && DigitNumber[1] == DigitNumber[3])
   
   {
     Console.WriteLine($"Ваше число: {DigitNumber} - палиндром.");
