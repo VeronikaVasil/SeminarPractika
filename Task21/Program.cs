a8c9a56f038b73614f53e971c3f6a674440c78ca
Veronika <valeria14081981@yandex.com>
2022-08-12 12:58:41 +0300

    Расстояние между жвумя точками в 3D

---
+++ Task21/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Write("Введите координату Ax: "); 
+int ax = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите координату точки Ay: "); 
+int ay = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите координату точки Az: ");
+int az = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки Bx: "); 
+int bx = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки By: ");
+int by = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки Bz: ");
+int bz = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt((ax - bx) * (ax - bx)) + ((ay - by) * (ay - by)) + ((az - bz) * (az - bz));
+
+Console.Write(Math.Round(distance, 3));
+
+
