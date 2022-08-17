3a4429973eef4c77784013bd7f47ba256b14f56f
Veronika <valeria14081981@yandex.com>
2022-08-17 09:55:51 +0300

    Массив из 8 рандомных  элементов

---
+++ Task29/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 29: Напишите программу, 
+//которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < 8; i++)
+{
+    array[i] = rand.Next(0, 9);
+    Console.Write(array[i] + " ");
+}
+Console.ReadLine();
+
