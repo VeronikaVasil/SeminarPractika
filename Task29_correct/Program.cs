089634bd033d814f1e077805d2aaf73b6c1a41c9
Veronika <valeria14081981@yandex.com>
2022-08-19 13:40:41 +0300

    Массив из 8 элементов произвольного размера

---
+++ Task29_correct/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Напишите программу,
+//которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+Random x = new Random();
+
+for (int i = 0; i < 8; i++)
+
+{
+    array[i] = x.Next();
+    Console.WriteLine(array[i] + " ");
+}
+
+Console.ReadLine();
