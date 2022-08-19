aa126d5a567a61a8fd7852d4a0c82ea5330f3769
Veronika <valeria14081981@yandex.com>
2022-08-19 13:52:40 +0300

    Цикл, который принимает число А и возводит в натуральную степень числа В

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int step = numberA;
+
+for(int i = 1; i < numberB; i++)
+{
+step = step * numberA;
+}
+
+Console.WriteLine($"{numberA} ^ {numberB} = {+ step}");
