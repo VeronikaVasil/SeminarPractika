b415a6ce713928952be594f55a7de5f1d1ffbb53
Veronika <valeria14081981@yandex.com>
2022-08-19 13:26:49 +0300

    Find the sum of digits in a number

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 27: Напишите программу, 
+//которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+while (N > 0)
+{
+    int number = N % 10;
+    N = N / 10;
+    sum = sum + number;
+}
+Console.WriteLine($"Сумма цифр в числе равна: {+sum}");
+
+Console.ReadLine();
+
+
+
+
+
