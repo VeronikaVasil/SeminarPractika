cd46ecf2832f728343f1da215cc5b4d84f2efa51
Veronika <valeria14081981@yandex.com>
2022-08-20 21:10:17 +0300

    Нахождение 2 цифры в трёхзначном числе путём подсчёта,в ином случае число не является трёхзначным

---
+++ Task10_02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Принять на вход трёхзначное число и на выходе показать вторую цифру этого числа.
+
+Console.Write("Введи трёхзначное число: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+if (threeDigitNumber < 1000 && threeDigitNumber > 99)
+{
+int twoDigitNumber = threeDigitNumber / 10;
+int result = twoDigitNumber % 10;
+Console.WriteLine($"У числа {threeDigitNumber} вторая цифра {result}");
+}
+
+else 
+{
+    Console.WriteLine("Число не является трёхзначным");
+}
+Console.ReadLine();
+
+
+
+
