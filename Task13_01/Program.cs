12984a0f99e44afcc3464e42773aa0c61b298149
Veronika <valeria14081981@yandex.com>
2022-08-21 01:43:56 +0300

    Найти 2 цифру любого введённого числа

---
+++ Task13_01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти вторую цифру введённого числа
+
+Console.Write("Введите число ");
+int k = Convert.ToInt32(Console.ReadLine());
+{
+    while (k >= 100) k /= 10;
+    int result = k % 10;
+    Console.WriteLine($"Second number is {result}"); 
+}
+Console.ReadLine();
+
