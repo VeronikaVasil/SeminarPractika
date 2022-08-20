8fc5373dd8e510adaf0186781d77ccc48ab9e467
Veronika <valeria14081981@yandex.com>
2022-08-20 23:44:56 +0300

    Трёхзначное число разбито на цифры при помощи строки

---
+++ Task10_01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Принять на вход трёхзначное число и на выходе показать 1,2,3 цифры этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string str = threeDigitNumber.ToString();
+Console.WriteLine("Первая цифра этого числа: {0} ", (str[0]));
+Console.WriteLine("Bторая цифра этого числа: {0} ", (str[1]));
+Console.WriteLine("Третья цифра этого числа: {0} ", (str[2]));
+
+
