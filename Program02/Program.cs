cfba0403cc3eea7a4bb952aa08fe4ac1937cebba
Veronika <valeria14081981@yandex.com>
2022-08-20 19:48:43 +0300

    Maксимальное число из введённых трёх чисел

--- Program02/Program.cs
+++ Program02/Program.cs
@@ -7,29 +7,23 @@ Console.WriteLine("Введите три числа: ");
 a = Convert.ToInt32(Console.ReadLine());
 b = Convert.ToInt32(Console.ReadLine());
 c = Convert.ToInt32(Console.ReadLine());
- 
-   if (b < a && a > c)
 
-   {
+if (b < a && a > c)
 
-       Console.WriteLine("{0} максимальное число", a);
-    
-     }
+{
+  Console.WriteLine("{0} максимальное число", a);
+}
 
-     else if (a < b && b > c)
-     {
+else if (a < b && b > c)
+{
+  Console.WriteLine("{0} максимальное число", b);
+}
 
-       Console.WriteLine("{0} максимальное число", b);
-    
-     }
+else if (a < c && c > b)
+{
+    Console.WriteLine("{0} максимальное число", c);
+}
 
-     else if (a < c && c > b)
-     {
-
-     Console.WriteLine("{0} максимальное число", c);
-    
-     }
-      
-     Console.ReadLine();
+Console.ReadLine();
 
 
