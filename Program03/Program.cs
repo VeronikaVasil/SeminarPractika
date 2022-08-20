63fc27a048c311f9e23eae8bf1b3767e25dd2538
Veronika <valeria14081981@yandex.com>
2022-08-20 19:40:53 +0300

    Определить чётное или нечётное введённое число

--- Program03/Program.cs
+++ Program03/Program.cs
@@ -1,28 +1,17 @@
 ﻿// Ввести число и определить, является ли оно четным или нечетным
 
 int x;
- 
-            Console.Write("Введите число: ");
- 
-            x = Convert.ToInt32(Console.ReadLine());
- 
-            if (x % 2 == 0)
- 
-            {
- 
-                Console.Write("Введенное число является четным");
- 
-                Console.Read();
- 
-            }
- 
-            else
- 
-            {
- 
-                Console.Write("Введенное число нечетное");
- 
-                Console.Read();
- 
-            }
-            
+Console.Write("Введите число: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.Write("Введенное число является четным");
+    Console.Read();
+}
+
+else
+{
+    Console.Write("Введенное число нечетное");
+    Console.Read();
+}
