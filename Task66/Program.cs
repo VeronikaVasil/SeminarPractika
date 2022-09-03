c2f66f175af6431efe15b907133d7059dac23374
Veronika <valeria14081981@yandex.com>
2022-09-03 03:44:37 +0300

    SummNumbers с помощью рекурсии

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+Console.Write("Введите numberM: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите numberN: ");
+int N = int.Parse(Console.ReadLine());
+
+int Summ (int M, int N)
+{
+ if (M < N) return N + Summ(M, N - 1);
+ return M;
+}
+int summ = Summ(M, N);
+Console.Write($"SummNumbers = {summ} ");
+
