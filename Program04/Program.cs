0101e0f35f23126b54fa5a32dbfeef82113c515b
Veronika <valeria14081981@yandex.com>
2022-09-05 17:06:24 +0300

    Input number N. Показать чётные числа от 1 до N

--- Program04/Program.cs
+++ Program04/Program.cs
@@ -1,18 +1,12 @@
 ﻿// Ввести число N, а на выводе показать все четные числа от 1 до N
 
-Console.WriteLine("Введите число N: ");
-
-int numberN = Convert.ToInt32(Console.ReadLine());
-    
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine()); 
     for (int i = 1;  i <= numberN; i++)
-
     {
         if (i % 2 == 0) 
-
         {
             Console.Write(i + " ");
         }
-
     }
-
-    Console.ReadLine();
+Console.ReadLine();
