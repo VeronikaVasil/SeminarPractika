044ca783030c4c5e2722722ccb7f1a77ec377ed7
Veronika <valeria14081981@yandex.com>
2022-09-05 17:19:17 +0300

    SecondDigit of threeDigitNumder

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,13 +1,10 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
 
-int a;
-
+int threeDigitNumber;
 Console.Write("Введите трёхзначное число: ");
 
-a = Convert.ToInt32(Console.ReadLine());
+threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string str = threeDigitNumber.ToString();
 
-string str = a.ToString();
-
-Console.Write("{0} вторая цифра данного числа", (str[1]));
-
+Console.Write($"{(str[1])} вторая цифра данного числа");
 Console.ReadLine();
