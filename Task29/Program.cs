ec0dfab56c484d280b4b20c7a227c57a89636ee2
Veronika <valeria14081981@yandex.com>
2022-09-05 17:56:04 +0300

    Array [8]

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -6,8 +6,7 @@ int[] array = new int[8];
 Random rand = new Random();
 for (int i = 0; i < 8; i++)
 {
-    array[i] = rand.Next(0, 9);
+    array[i] = rand.Next(0, 10);
     Console.Write(array[i] + " ");
 }
 Console.ReadLine();
-
