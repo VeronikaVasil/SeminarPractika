7545798ace8a1560b844b17e5e91c0a20f6c0e0f
Veronika <valeria14081981@yandex.com>
2022-09-05 18:05:15 +0300

    Точка пересечения двух прямых

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -4,13 +4,10 @@
 
 Console.Write("Введите число b1:  ");
 double b1 = Convert.ToDouble(Console.ReadLine());
-
 Console.Write("Введите число k1:  ");
 double k1 = Convert.ToDouble(Console.ReadLine());
-
 Console.Write("Введите число b2:  ");
 double b2 = Convert.ToDouble(Console.ReadLine());
-
 Console.Write("Введите число k2:  ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 {
@@ -24,8 +21,8 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 
    else
    {
-    xIntersection = (b2 - b1) / (k1 - k2);
-    yIntersection = k1 * xIntersection + b1;
+    xIntersection = (-b2 + b1) / (-k1 + k2);
+    yIntersection = k2 * xIntersection + b2;
     Console.WriteLine($"Точка пересечения двух прямых имеет координаты x: {xIntersection}, y: {yIntersection}");
    }
  }
